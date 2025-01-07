d2c20855124f5d2f77242a2ad0875bd1424a44fe
eachinlee <lee.echin@gmail.com>
2025-01-07 22:40:58 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -14,17 +14,10 @@ gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
-    #"""
-    #Setup Prompt  
-    prompt_tmp = PromptTemplate.from_template("以下列格式輸出{format_set}，且注意此條件{condition_set}")
-    prompt_str = prompt_tmp.format(
-    format_set="請生成一個 JSON 物件，包含一個名為 Result的屬性，該屬性下有兩個子屬性： date :YYYY-MM-DD  和 name :紀念日名稱",
-    condition_set="最後不需顯示任何其他資訊")
 
-    question = question + prompt_str
-    #"""
+    question = question + "以JSON格式輸出外層為Result:，每筆資料包含date:只需要顯示日期與name:顯示紀念日名稱，以西元年開頭YYYY-MM-DD 格式顯示日期，再換行顯示紀念日名稱，最後不需顯示任何其他注意資訊，"
+    #print(question)
 
-    #Use Demo function to process LLM API
     llm = AzureChatOpenAI(
             model=gpt_config['model_name'],
             deployment_name=gpt_config['deployment_name'],
@@ -40,16 +33,10 @@ def generate_hw01(question):
     )
     response = llm.invoke([message])
 
-    #print response as JSON object
-    #print(response.content)
-
-    #try to apply JsonOutputParser()
-
-    json_parser = JsonOutputParser()
-    json_output = json_parser.invoke(response)
-    print(json_output)
+    print(response.content)
 
     return response
+    
     
 
 
