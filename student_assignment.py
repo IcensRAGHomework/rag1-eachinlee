cade30a6228642ff6468066adb71bd85a1e93629
eachinlee <lee.echin@gmail.com>
2025-01-08 00:24:34 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -13,7 +13,7 @@ gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
 
-    question = question + "請生成一個 JSON 物件，不需要顯示json字串，直接包含一個名為 Result的屬性，該屬性下有兩個子屬性： date :YYYY-MM-DD  和 name :紀念日名稱，不需顯示任何其他資訊，"
+    question = question + "請生成一個 JSON 物件，不需要顯示json字串，直接包含一個名為 Result的屬性，該屬性下有兩個子屬性： date :YYYY-MM-DD  和 name :紀念日名稱，不含台灣光復節，，不需顯示任何其他資訊，"
     #print(question)
 
     llm = AzureChatOpenAI(
