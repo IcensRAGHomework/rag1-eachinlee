45085c8100e84bf4dc3b8c6605e0c7cb2f3b8c6e
eachinlee <lee.echin@gmail.com>
2025-01-07 11:09:09 +0800

    HW#01
    HW#01

--- student_assignment.py
+++ student_assignment.py
@@ -1,5 +1,6 @@
 import json
 import traceback
+import re
 
 from model_configurations import get_model_configuration
 
@@ -10,18 +11,10 @@ gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
-    pass
-    
-def generate_hw02(question):
-    pass
-    
-def generate_hw03(question2, question3):
-    pass
-    
-def generate_hw04(question):
-    pass
-    
-def demo(question):
+
+    question = question + "以JSON格式輸出外層為Result:，每筆資料包含date:只需要顯示日期與name:顯示紀念日名稱，以西元年開頭YYYY-MM-DD 格式顯示日期，再換行顯示紀念日名稱，最後不需顯示任何其他注意資訊，"
+    #print(question)
+
     llm = AzureChatOpenAI(
             model=gpt_config['model_name'],
             deployment_name=gpt_config['deployment_name'],
@@ -36,5 +29,54 @@ def demo(question):
             ]
     )
     response = llm.invoke([message])
-    
+
+    print(response.content)
+
     return response
+    
+
+
+def generate_hw02(question):
+    pass
+    
+def generate_hw03(question2, question3):
+    pass
+    
+def generate_hw04(question):
+    pass
+    
+def demo(question):
+
+    llm = AzureChatOpenAI(
+            model=gpt_config['model_name'],
+            deployment_name=gpt_config['deployment_name'],
+            openai_api_key=gpt_config['api_key'],
+            openai_api_version=gpt_config['api_version'],
+            azure_endpoint=gpt_config['api_base'],
+            temperature=gpt_config['temperature']
+    )
+    message = HumanMessage(
+            content=[
+                {"type": "text", "text": question},
+            ]
+    )
+    response = llm.invoke([message])
+
+    #print(response.content)
+
+    return response
+
+
+
+
+
+
+#Test generate_hw01
+print("generate_hw01 請回答台灣特定月份的紀念日有哪些(請用JSON格式呈現)?")
+QQ="2024年台灣5月紀念日有哪些?"
+print(QQ)
+RR = generate_hw01(QQ)
+
+#RR = demo(QQ)
+#print(RR.content)
+
